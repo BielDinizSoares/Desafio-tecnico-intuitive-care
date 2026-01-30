992de8be8b4e6b776a441b0a20fe269d4ca99754
Gabriel Diniz <bieldinizsoares2016@gmail.com>
2026-01-30 12:05:08 -0300

    feat: Descompacta e le arquivos, cria unico DF para eventos/sinistros

--- descompacta.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import zipfile
-
-files_to_unzip = ['data/1T2025.zip', 'data/2T2025.zip', 'data/3T2025.zip']
-
-def descompactar_arquivos(): 
-    for file in files_to_unzip:
-        with zipfile.ZipFile(file, 'r') as zip_ref:
-            zip_ref.extractall('data')
